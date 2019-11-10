4356f6eb9384b2e61474bf355499fc9bbc2bf3c7
Pranavi Boyalakuntla <naviatolin@gmail.com>
2019-11-10 15:14:47 -0500

    Add sounds

---
+++ Firmware/sound.py
@@ -0,0 +1,33 @@
+import pygame
+
+def talk(state):
+    pygame.mixer.init()
+    if state == "batt":
+        pygame.mixer.music.load("batt.wav")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
+    
+    elif state == "hi":
+        pygame.mixer.music.load("hi.wav")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
+    
+    elif state == "bye":
+        pygame.mixer.music.load("bye.wav")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
+    
+    elif state == "help":
+        pygame.mixer.music.load("help.wav")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
+    
+    else:
+        pygame.mixer.music.load("confused.wav")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
