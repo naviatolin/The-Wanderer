672c24a4d37f14abdef07b3ebfcc90ca98803dc2
Case Zito <casezito@gmail.com>
2019-11-18 15:53:43 -0500

    Modify code to use new Servo motors
    Commented or deleted unneeded code

--- Firmware/wheel_run/wheel_run.ino
+++ Firmware/wheel_run/wheel_run.ino
@@ -1,77 +1,69 @@
 //import libraries
 #include <Servo.h>
 #include <Wire.h>
-#include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_MS_PWMServoDriver.h"
+//#include "utility/Adafruit_MS_PWMServoDriver.h"
 
-//create instances of motor class
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor *leftMotor = AFMS.getMotor(4);
-Adafruit_DCMotor *rightMotor = AFMS.getMotor(3);
-
-//servo
-Servo headServo;
-int angle = 0;
+//create instances of Servo class for legs
+Servo leftLeg;
+Servo rightLeg;
 //variables for motor speed and sensors
-int generalSpeed = 50; //starting speed for motors
-int distance = 5000; //large distance so it doesn't set off stop
-//for inputing speeds later
+int generalSpeed = 180; //starting speed for motors
+
+//prox sensor
 #define prox_sensor A0
-String incomingVal = "";
-int s = 0;
+int distance = 5000; //large distance so it doesn't set off stop yet
+
+//String incomingVal = "";
+//int s = 0;
+
 //Raspi communication
 int cameraPin = A0; //11
 int stopPin = A1;
 
 void setup() {
-  AFMS.begin();
-  headServo.attach(9);
-  //pinMode(LED_BUILTIN, OUTPUT);
+  leftLeg.attach(9);
+  rightLeg.attach(10);
   pinMode(cameraPin, OUTPUT);
   pinMode(stopPin, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  headServo.write(angle);
   //allows us to change default speed of robot without recompiling code
-  if (Serial.available() > 0) { //if there's bytes available
-    incomingVal = Serial.readString(); //read the incoming byte
-    s = (incomingVal.toInt()); //convert it to an int
-    angle = s + angle; //change speed based on input
-    headServo.write(angle);
-    Serial.println(angle);
-  }
+  //if (Serial.available() > 0) { //if there's bytes available
+  //  incomingVal = Serial.readString(); //read the incoming byte
+  //  s = (incomingVal.toInt()); //convert it to an int
+  //  angle = s + angle; //change speed based on input
+  //  headServo.write(angle);
+  //  Serial.println(angle);
+  //}
 
   //robot's normal forward speed
-  rightMotor->run(FORWARD); //direction of motors
-  leftMotor->run(FORWARD);
+  
 
   if (analogRead(cameraPin)>=170){
     //Serial.println("person seen");
-    rightMotor ->setSpeed(0);
-    leftMotor ->setSpeed(0);    
+    leftLeg.write(0);
+    rightLeg.write(0);   
   }
-
   distance = analogRead(prox_sensor);
   //Serial.print("Distance: ");
   //Serial.println(distance);
   
   if (analogRead(stopPin)>= 170){ //don't move robot when speaker is being used
-    rightMotor ->setSpeed(0);
-    leftMotor ->setSpeed(0);
+    leftLeg.write(0);
+    rightLeg.write(0);
   }
   else if (distance > 400) {
-    //Serial.println("Turning, distance > 350");
-    leftMotor ->setSpeed(generalSpeed+20); //left motor is slightly slower than right
-    rightMotor ->setSpeed(0);
+    //Serial.println("Turning, distance > 400");
+    leftLeg.write(generalSpeed+20);
+    rightLeg.write(0);
   }
   else{
-    leftMotor ->setSpeed(generalSpeed+5); //left motor is slightly slower than right
-    rightMotor ->setSpeed(generalSpeed);
-    //Serial.print(String(generalSpeed));
-    //Serial.print(", ");
-    //Serial.println(String(generalSpeed));
+    leftLeg.write(generalSpeed);
+    rightLeg.write(generalSpeed);
+    //Serial.println("Both running");
+    //Serial.println(generalSpeed);
   }
 
 }
