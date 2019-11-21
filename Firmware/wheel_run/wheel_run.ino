fb4e2501bab52199778606c723e1a61b2246473a
Case Zito <casezito@gmail.com>
2019-11-21 17:07:00 -0500

    Update code to stop based on cameraPin
    Ignore stopPin since that connection was not working correctly. This code is working for movement (stopping and going at right times).

--- Firmware/wheel_run/wheel_run.ino
+++ Firmware/wheel_run/wheel_run.ino
@@ -19,13 +19,13 @@ int distance = 5000; //large distance so it doesn't set off stop yet
 
 //Raspi communication
 int cameraPin = A1; //11
-int stopPin = A2;
+//int stopPin = A2;
 
 void setup() {
   leftLeg.attach(9);
   rightLeg.attach(10);
   pinMode(cameraPin, OUTPUT);
-  pinMode(stopPin, OUTPUT);
+  //pinMode(stopPin, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -42,33 +42,33 @@ void loop() {
   //robot's normal forward speed
   delay(1000);
   //Serial.print("Seen: ");
-  //Serial.print(analogRead(cameraPin)>=170);
+  //Serial.println(analogRead(cameraPin)>=170);
   //Serial.print(", Held: ");
   //Serial.println(analogRead(stopPin));
   
-  if (analogRead(cameraPin)>=170){
-    Serial.println("Person seen, stopping");
-    leftLeg.write(90); //90 is stop
-    rightLeg.write(90);   
-  }
+  //if (analogRead(cameraPin)>=170){
+  //  Serial.println("Person seen, stopping");
+  //  leftLeg.write(90); //90 is stop
+  //  rightLeg.write(90);   
+  //}
   distance = analogRead(prox_sensor);
   Serial.print("Distance: ");
   Serial.println(distance);
   
-  //if (analogRead(stopPin)> 0){ //don't move robot when speaker is being used
-  //  Serial.println("Stopped while talking");
-  //  leftLeg.write(90);
-  //  rightLeg.write(90);
-  //}
-  if (distance > 300) {
+  if (analogRead(cameraPin)>=170){ //don't move robot when speaker is being used
+    Serial.println("Stopped while talking");
+    leftLeg.write(90);
+    rightLeg.write(90);
+  }
+  else if (distance > 300) {
     Serial.println("Turning, distance > 400");
     leftLeg.write(140);//leftSpeed-20
     rightLeg.write(140); //rightSpeed+20
   }
-  else if (analogRead(stopPin)==0){
+  else if (analogRead(cameraPin)<170){
+    Serial.println("Both running");
     leftLeg.write(leftSpeed);
     rightLeg.write(rightSpeed);
-    Serial.println("Both running");
     //Serial.println(generalSpeed);
   }
 
