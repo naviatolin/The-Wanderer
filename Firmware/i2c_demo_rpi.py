a1b4174d0121e540b72956b7adae78d60cfe12a4
Michael <michaeljremley@gmail.com>
2019-11-21 23:42:00 -0500

    Update comp vis code stop robot after person is seen

--- Firmware/i2c_demo_rpi.py
+++ Firmware/i2c_demo_rpi.py
@@ -11,38 +11,48 @@ from sound import talk
 def main():
     engine = ProcessingEngine(debug=True)
     engine.turn_on()
+    previous = 0
     while True:
         gpio.setmode(gpio.BOARD) #gpio.BCM
         gpio.setup(36, gpio.OUT) # am I seeing people
         gpio.setup(40, gpio.OUT) # should I stop
         status = False
         status = engine.person_detected()
-        if status:
+        if status and previous:
             gpio.output(36, gpio.HIGH)
             time.sleep(2) # wait 2 seconds for the arduino to stop completely
 
             # say hi and don't move while you are speaking
             done_hi = talk("hi")
+            print("hi")
             while done_hi is 0:
                 gpio.output(40,gpio.HIGH)
+            
+            gpio.output(40, gpio.LOW)
 
             # say bye and don't move while you are speaking
+            print("bye")
             done_bye = talk("bye")
             while done_bye is 0:
                 gpio.output(40,gpio.HIGH)
             
             gpio.output(40, gpio.LOW)
-            
+            time.sleep(2)
+            previous = 0
         else:
             gpio.output(36, gpio.LOW)
             gpio.output(40, gpio.LOW)
+            previous = 1
 
-        time.sleep(1)
-        gpio.cleanup()
+        time.sleep(6)
+        #gpio.cleanup()
 
 if __name__ == '__main__':
     try:
+        #previous=1
+        print('running')
         main()
+        #gpio.cleanup()
 
     except KeyboardInterrupt:
         print ("Interrupted")
--- Firmware/sound.py
+++ Firmware/sound.py
@@ -3,25 +3,25 @@ import pygame
 def talk(state):
     pygame.mixer.init()
     if state == "batt":
-        pygame.mixer.music.load("batt.wav")
+        pygame.mixer.music.load("/home/pi/The-Wanderer/Firmware/sounds/batt.wav")
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy() == True:
             continue
     
     elif state == "hi":
-        pygame.mixer.music.load("hi.wav")
+        pygame.mixer.music.load("/home/pi/The-Wanderer/Firmware/sounds/hi.wav")
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy() == True:
             continue
     
     elif state == "bye":
-        pygame.mixer.music.load("bye.wav")
+        pygame.mixer.music.load("/home/pi/The-Wanderer/Firmware/sounds/bye.wav")
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy() == True:
             continue
     
     elif state == "help":
-        pygame.mixer.music.load("help.wav")
+        pygame.mixer.music.load("/home/pi/The-Wanderer/Firmware/sounds/help.wav")
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy() == True:
             continue
