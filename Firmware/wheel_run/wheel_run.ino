b84223a564e71aee857240383a8713d9f936cac3
Case Zito <casezito@gmail.com>
2019-12-05 23:09:26 -0500

    Add battery voltage converting function
    Clean up code

--- Firmware/wheel_run/wheel_run.ino
+++ Firmware/wheel_run/wheel_run.ino
@@ -1,7 +1,6 @@
 //import libraries
 #include <Servo.h>
 #include <Wire.h>
-//#include "utility/Adafruit_MS_PWMServoDriver.h"
 
 //create instances of Servo class for legs
 Servo leftLeg;
@@ -12,46 +11,41 @@ int rightSpeed = 180-leftSpeed;
 
 //prox sensor
 #define prox_sensor A0
-int distance = 5000; //large distance so it doesn't set off stop yet
-
-//String incomingVal = "";
-//int s = 0;
+int distance = 0;
 
 //Raspi communication
 int cameraPin = A1; //notification from raspi for seeing a person 
 int battInputPin = A2; //reads the current battery voltage
-int battOutputPin = 11; //outputs a battery low signal to raspi
+int battOutputPin = 8; //outputs a battery low signal to raspi
+float battVolt = 0;  //to track battery voltage
+
+float getBatteryVoltage(int batteryPin){
+  // Convert the analogRead to battery voltage
+  // 5V / 1024 to go from Arduino ADC to Measured Voltage
+  // *4.34 to go from Measured Voltage to Battery Voltage
+  // (Divider divides by 4.34)
+  return (float)analogRead(batteryPin) * 5 * 4.34 / 1024;
+}
 
 void setup() {
   leftLeg.attach(9);
   rightLeg.attach(10);
-  pinMode(cameraPin, INPUT);
-  pinMode(battInputPin, INPUT);
   pinMode(battOutputPin, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  delay(1000);
-
+  battVolt = getBatteryVoltage(battInputPin); // read battery voltage
   Serial.print("Battery reading: ");
-  Serial.print(analogRead(battInputPin)); //analog voltage is 0-1024 where 1024 is 5V
-  //low battery signal is for 4.8V or analog V 985
-  if (analogRead(battInputPin)<=985){
-    Serial.println(" -> Low Battery");
-    digitalWrite(battOutputPin, LOW); //low battery signal to raspi
-  }
-  else{
-    Serial.println(" -> Good Battery");
-    digitalWrite(battOutputPin, HIGH);
-  }
-  
-  distance = analogRead(prox_sensor);
+  Serial.println(battVolt);
+  digitalWrite(battOutputPin, battVolt < 11.5); //Indicate high if battery voltage is less than 11.5
+
   //Serial.print("Distance: ");
   //Serial.println(distance);
+  distance = analogRead(prox_sensor);
 
   //Serial.print("Seen: ");
-  //Serial.println(analogRead(cameraPin)>=170);
+  Serial.println(analogRead(cameraPin)>=170);
   if (analogRead(cameraPin)>=170){ // Robot stops when speaker is being used
     Serial.println("Stopped while talking");
     leftLeg.write(90);
@@ -63,11 +57,10 @@ void loop() {
     rightLeg.write(140);
   }
   else if (analogRead(cameraPin)<170){
-    Serial.println("Both running");
-    leftLeg.write(leftSpeed);
-    rightLeg.write(rightSpeed);
+    Serial.println("Going Forward");
     //Serial.print("Speed: ");
     //Serial.println(rightSpeed);
+    leftLeg.write(leftSpeed);
+    rightLeg.write(rightSpeed);
   }
-
 }
